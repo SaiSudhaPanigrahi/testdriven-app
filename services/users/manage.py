8b6231decb2cd78bbff42f55e83444a14ff3e8b5
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-05-22 14:53:39 +0200

    📦 NEW: add flask blueprints

--- services/users/manage.py
+++ services/users/manage.py
@@ -3,9 +3,11 @@ import unittest
 
 from flask.cli import FlaskGroup
 
-from project import app, db  # new
+from project import create_app, db  # new
+from project.api.models import User  # new
 
-cli = FlaskGroup(app)
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # new
 
 
 @cli.command('recreate_db')
