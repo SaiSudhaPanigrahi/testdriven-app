71adc3416a04625ed8111a4950582e31e999ff34
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-05-22 06:04:55 +0200

    📦 NEW: update using Flask-RESTful

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,14 +1,19 @@
 import os
 from flask import Flask, jsonify
+from flask_restful import Resource, Api
 
 # instantiate the app
 app = Flask(__name__)
 
+api = Api(app)
+
 # set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+app.config.from_object('project.config.DevelopmentConfig')
 
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({'status': 'success', 'message': 'pong!'})
+class UsersPing(Resource):
+    def get(self):
+        return {'status': 'success', 'message': 'pong!'}
+
+
+api.add_resource(UsersPing, '/users/ping')
