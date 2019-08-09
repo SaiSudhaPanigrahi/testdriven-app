16a97ecee93b6450b02dbe9be3f7c48e4406ed63
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-08-09 12:16:12 +0200

    📦 NEW: added user auth create route

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -36,6 +36,10 @@ def create_app(script_info=None):
 
     app.register_blueprint(users_blueprint)
 
+    from project.api.auth import auth_blueprint
+
+    app.register_blueprint(auth_blueprint)
+
     # shell context for flask cli
     @app.shell_context_processor
     def ctx():
