9e7ec93b7426520897349faccc7f510a0e1e31f3
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-08-09 22:48:07 +0200

    📦 NEW: added login route

--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -118,34 +118,32 @@ class TestAuthBlueprint(BaseTestCase):
             self.assertIn("Invalid payload.", data["message"])
             self.assertIn("fail", data["status"])
 
-    # def test_registered_user_login(self):
-    #     with self.client:
-    #         add_user("test", "test@test.com", "testpass123")
-    #         response = self.client.post(
-    #             "/auth/login",
-    #             data=json.dumps(
-    # {"email": "test@test.com", "password": "testpass123"}),
-    #             content_type="application/json",
-    #         )
-    #         data = json.loads(response.data.decode())
-    #         self.assertTrue(data["status"] == "success")
-    #         self.assertTrue(data["message"] == "Successfully logged in.")
-    #         self.assertTrue(data["auth_token"])
-    #         self.assertTrue(response.content_type == "application/json")
+    def test_registered_user_login(self):
+        with self.client:
+            add_user("test", "test@test.com", "testpass123")
+            response = self.client.post(
+                "/auth/login",
+                data=json.dumps({"email": "test@test.com", "password": "testpass123"}),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data["status"] == "success")
+            self.assertTrue(data["message"] == "Successfully logged in.")
+            self.assertTrue(data["auth_token"])
+            self.assertTrue(response.content_type == "application/json")
 
-    # def test_not_registered_user_login(self):
-    #     with self.client:
-    #         response = self.client.post(
-    #             "/auth/login",
-    #             data=json.dumps(
-    # {"email": "test@test.com", "password": "testpass123"}),
-    #             content_type="application/json",
-    #         )
-    #         data = json.loads(response.data.decode())
-    #         self.assertEqual(response.status_code, 400)
-    #         self.assertTrue(response.content_type == "application/json")
-    #         self.assertIn("Invalid credentials.", data["message"])
-    #         self.assertIn("fail", data["status"])
+    def test_not_registered_user_login(self):
+        with self.client:
+            response = self.client.post(
+                "/auth/login",
+                data=json.dumps({"email": "test@test.com", "password": "testpass123"}),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertTrue(response.content_type == "application/json")
+            self.assertIn("Invalid credentials.", data["message"])
+            self.assertIn("fail", data["status"])
 
 
 if __name__ == "__main__":
