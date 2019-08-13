28f6510c02c75a485b164ae734bc6d28c857801a
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-08-13 18:07:31 +0200

    📦 NEW: added test for inactive user logout

--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -3,8 +3,8 @@ import unittest
 
 from flask import current_app
 
-# from project import db
-# from project.api.models import User
+from project import db
+from project.api.models import User
 from project.tests.base import BaseTestCase
 from project.tests.utils import add_user
 
@@ -198,6 +198,28 @@ class TestAuthBlueprint(BaseTestCase):
             self.assertTrue(response.content_type == "application/json")
             self.assertTrue(data["status"] == "fail")
             self.assertTrue(data["message"] == "Invalid token. Please login again.")
+
+    def test_invalid_logout_inactive(self):
+        add_user("test", "test@test.com", "testpass123")
+        # update user
+        user = User.query.filter_by(email="test@test.com").first()
+        user.active = False
+        db.session.commit()
+        with self.client:
+            resp_login = self.client.post(
+                "/auth/login",
+                data=json.dumps({"email": "test@test.com", "password": "testpass123"}),
+                content_type="application/json",
+            )
+            token = json.loads(resp_login.data.decode())["auth_token"]
+            response = self.client.get(
+                "/auth/logout", headers={"Authorization": f"Bearer {token}"}
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 401)
+            self.assertTrue(response.content_type == "application/json")
+            self.assertTrue(data["status"] == "fail")
+            self.assertTrue(data["message"] == "Provide a valid auth token.")
 
     def test_user_status(self):
         add_user("test", "test@test.com", "testpass123")
