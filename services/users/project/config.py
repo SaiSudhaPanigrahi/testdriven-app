c05ccb7ddb9dfa466529f9ddfc8450ba46d1f3a0
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-12-23 07:41:04 +0100

    📦 NEW: staging environment

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -36,3 +36,9 @@ class ProductionConfig(BaseConfig):
     """Production configuration"""
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class StagingConfig(BaseConfig):
+    """Staging configuration"""
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
